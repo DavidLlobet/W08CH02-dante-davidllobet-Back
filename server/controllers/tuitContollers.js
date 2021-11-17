96ca0398c2bda4f0a72da8fa12febd9f27e14ab4
Dante Allievi <danteallievi@gmail.com>
2021-11-17 19:27:58 +0100

    Tuit delete controller added

--- server/controllers/tuitContollers.js
+++ server/controllers/tuitContollers.js
@@ -3,11 +3,26 @@ const Tuit = require("../../DB/models/Tuit");
 const getTuits = async (req, res, next) => {
   try {
     const tuits = await Tuit.find();
-    res.status(200).json(tuits);
+    return res.status(200).json(tuits);
   } catch {
     const error = new Error("Error loading the tuits.");
-    next(error);
+    return next(error);
   }
 };
 
-module.exports = { getTuits };
+const deleteTuit = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const tuit = await Tuit.findByIdAndDelete(id);
+    if (!tuit) {
+      const error = new Error("Tuit not found.");
+      return next(error);
+    }
+    return res.status(200).json(id);
+  } catch {
+    const error = new Error("Error deleting the tuit.");
+    return next(error);
+  }
+};
+
+module.exports = { getTuits, deleteTuit };
